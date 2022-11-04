b4e0f201119c322224e9332568183181b04c6f9d
Samir000777 <111176325+Samir000777@users.noreply.github.com>
2022-11-04 07:52:05 +0300

    wew
    ewe

---
+++ app.js
@@ -0,0 +1,17 @@
+let cars = [16000, 40000, 3000, 1500, 390000, 12000, 333000, 8000, 714000, 22000, 300, 180000, 13000, 9000]
+let notExpensive = []
+console.log(
+cars.filter(cnotExpensive => notExpensive <= 35000)
+);
+
+
+// //2
+// let arr = [4, 2, 5, 8, 4, 46, 12, 9, 27, 8, 33, 0, 9]
+// let even = []
+// let odd = []
+// console.log(
+// arr.filter(odd => odd % 2)
+// );
+// console.log(
+// arr.filter(even => even % 3 )
+// );
